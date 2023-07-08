f6551d5c6eba304c33682c7efc1b8e7fcdd4c5c3
MANIDEEP <MANIDEEP@LAPTOP-0T4925AO>
2023-07-07 19:00:22 -0500

    Add project files.

---
+++ ipermit system/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ipermit_system.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
